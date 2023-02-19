c718c75676e072ed69db81099f3ff4c29d321935
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2023-02-19 19:18:59 +0100

    Shadow buffer: Create the shadow_depth from Light point of view. Need to figure out how to compare the values with camera point of view.

--- header_files/draw_functions.h
+++ header_files/draw_functions.h
@@ -33,17 +33,17 @@
     #include <stdlib.h>
 #endif
 
-const void drawline(Pixel **buffer, float x1, float y1, float x2, float y2, const float red, const float green, const float blue);
+const void drawLine(Pixel **buffer, float x1, float y1, float x2, float y2, const float red, const float green, const float blue);
 
-const void filltriangle(Pixel **pixels, float **depth_buffer, Triangle *t, Phong model);
-const void fillnorthway(Pixel **pixels, float **depth_buffer, const Triangle t, Phong model, const float winding);
-const void fillsouthway(Pixel **pixels, float **depth_buffer, const Triangle t, Phong model, const float winding);
-const void fillgeneral(Pixel **pixels, float **depth_buffer, const Triangle t, Phong model, const float winding);
+const void fillTriangle(Pixel **pixels, float **depth_buffer, float **shadow_buffer, Triangle *t, Phong model);
+const void fillNorthway(Pixel **pixels, float **depth_buffer, const Triangle t, Phong model, const float winding);
+const void fillSouthway(Pixel **pixels, float **depth_buffer, const Triangle t, Phong model, const float winding);
+const void fillGeneral(Pixel **pixels, float **depth_buffer, float **shadow_buffer, const Triangle t, Phong model, const float winding);
 
-const void textriangle(Pixel **pixels, float **depth_buffer, Triangle *t, const float light, Pixel **texels, const int tex_height, const int tex_width);
-const void texnorthway(Pixel **pixels, float **depth_buffer, const Triangle t, const float light, const float winding, Pixel **texels, const int tex_height, const int tex_width);
-const void texsouthway(Pixel **pixels, float **depth_buffer, const Triangle t, const float light, const float winding, Pixel **texels, const int tex_height, const int tex_width);
-const void texgeneral(Pixel **pixels, float **depth_buffer, const Triangle t, const float light, const float winding, Pixel **texels, const int tex_height, const int tex_width);
+const void texTriangle(Pixel **pixels, float **depth_buffer, Triangle *t, const float light, Pixel **texels, const int tex_height, const int tex_width);
+const void texNorthway(Pixel **pixels, float **depth_buffer, const Triangle t, const float light, const float winding, Pixel **texels, const int tex_height, const int tex_width);
+const void texSouthway(Pixel **pixels, float **depth_buffer, const Triangle t, const float light, const float winding, Pixel **texels, const int tex_height, const int tex_width);
+const void texGeneral(Pixel **pixels, float **depth_buffer, const Triangle t, const float light, const float winding, Pixel **texels, const int tex_height, const int tex_width);
 
 #endif /* _DRAW_FUNCTIONS_H */
 
